92431c0ad1cb10f30a82850b2dd5a0877861e293
Rohan Maisuria <rohanmaisuria@Rohans-MacBook-Air.local>
2023-10-15 17:50:44 -0700

    first push on gym web app using typescript

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import Navbar from "@/scenes/navbar";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
+import Home from "./scenes/Home";
+import Benefits from "./scenes/Benefits";
+import OurClasses from "./scenes/OurClasses.tsx";
+import Footer from "./scenes/footer/index.tsx";
+import ContactUs from "./scenes/contactUs/index.tsx";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        //checking if we are at the top of the page
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      } else {
+        setIsTopOfPage(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="bg-gray-20">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
